cfa0e7d9173b0358a86dba144c299f8fefe8faaf
Galin7777 <kazakova0309@yandex.ru>
2025-01-06 16:18:01 +0300

    Added navigation

--- src/widgets/Header/Header.js
+++ src/widgets/Header/Header.js
@@ -1,21 +1,30 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.scss';
 
 /**
  * @function Header
  * @returns {JSX.Element}
  */
+
 export const Header = () => {
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
         <div className={classes.left}>
-          <Link to="/">HomePage</Link>
+          <NavLink to="/" className={({ isActive }) => isActive ? classes.active : undefined}>
+            HomePage
+          </NavLink>
         </div>
         <div className={classes.right}>
-          <Link to="/photos">Photos</Link>
-          <Link to="/todos">Todos</Link>
-          <Link to="/posts">Posts</Link>
+          <NavLink to="/photos" className={({ isActive }) => isActive ? classes.active : undefined}>
+            Photos
+          </NavLink>
+          <NavLink to="/todos" className={({ isActive }) => isActive ? classes.active : undefined}>
+            Todos
+          </NavLink>
+          <NavLink to="/posts" className={({ isActive }) => isActive ? classes.active : undefined}>
+            Posts
+          </NavLink>
         </div>
       </nav>
     </header>
